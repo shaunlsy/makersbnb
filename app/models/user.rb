9396d7275b9f183d5b789f98af63051914960d46
cpcwood <cpcwood@outlook.com>
2020-01-28 12:21:00 +0000

    Authenticate method added to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User
 
   attr_reader :email
 
+  def initialize(username, email)
+    @username = username
+    @email = email
+  end
+
   def self.setup(dbname)
     @dbconnection = dbname
   end
@@ -13,10 +18,18 @@ class User
     @dbconnection.command("INSERT INTO users(username, email, password) VALUES('#{username}', '#{email}', '#{encrypted_password}');")
   end
 
-  def initialize(username, email)
-    @username = username
-    @email = email
+  def self.authenticate(email, password)
+    begin
+      database_password = @dbconnection.command("SELECT password FROM users WHERE email='#{email}';")[0]['password']
+    rescue
+      return nil
+    end
+    if BCrypt::Password.new(database_password) == password
+      user_id = @dbconnection.command("SELECT user_id FROM users WHERE email='#{email}';")[0]['user_id']
+      return user_id
+    else
+      return nil
+    end
   end
-
 
 end
