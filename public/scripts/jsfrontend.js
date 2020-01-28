fda4cfe92c51db611c7a099c00343048704e5d89
cpcwood <cpcwood@outlook.com>
2020-01-28 23:23:58 +0000

    Layout fixed

--- public/scripts/jsfrontend.js
+++ public/scripts/jsfrontend.js
@@ -1,21 +1,26 @@
 "use strict"
+
 window.addEventListener("load", function() {
-    $(document).ready(function() {
-        $(".log-in-button").click(function() {
-            $("#log-in").slideToggle();
-        })
-
-        $(".sign-up-button").click(function() {
-            $("#sign-up").slideToggle();
-        })
-
-        $(".listing").click(function() {
-            var id = $(this).attr('id')
-            $.get(`listing/${id}`, function(data) {
-              $(`#extra${id}`).html(data)
-            })
-        })
-
-
+  $(document).ready(function() {
+    $(".log-in-button").click(function() {
+      $("#log-in").slideToggle();
     })
+
+    $(".sign-up-button").click(function() {
+      $("#sign-up").slideToggle();
+    })
+
+    $(".listing").click(function() {
+      var id = $(this).attr('id')
+      $.get(`listing/${id}`, function(data) {
+        $(`#extra${id}`).html(data)
+      })
+    })
+
+    // $(".extra").click(function() {
+    //   var id = $(this).attr('id')
+    //   $(`#${id}`).toggle()
+    // })
+
+  })
 })
